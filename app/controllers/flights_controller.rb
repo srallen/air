5bef65f81014133b98e5e6624015fa86bf877c59
Jeff Cohen <jeff@starterleague.com>
2013-09-03 14:56:49 -0500

    Enable search functionality

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,17 @@
 class FlightsController < ApplicationController
 
   def index
-    @flights = Flight.order('departs_at').limit(50)
+    @flights = Flight.order('departs_at')
+
+    if params[:departs_from]
+      @flights = @flights.where('departure_code = ?', params[:departs_from])
+    end
+
+    if params[:departs_on]
+      @flights = @flights.where('DATE(departs_at) = ?', params[:departs_on])
+    end
+
+    @flights = @flights.limit(50)
   end
 
 end
