2d02cf88bce0a66a186deb155d0a59046bbee8e1
Sarah Allen <sarah.sra1@gmail.com>
2013-09-12 15:41:58 -0500

    Read Rails guide on active record querying and where conditionals. Corrected .where condition in flight controller. Date still needs correction.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,11 +4,11 @@ class FlightsController < ApplicationController
     @flights = Flight.order('departs_at')
 
     if params[:departs_from]
-      @flights = @flights.where('departure_code' => params[:departs_from])
+      @flights = @flights.where('departure_code = ?', params[:departs_from])
     end
 
     if params[:departs_on]
-      @flights = @flights.where('departs_at' => params[:departs_on])
+      @flights = @flights.where('departs_at = ?', params[:departs_on])
     end
 
     @flights = @flights.limit(50)
