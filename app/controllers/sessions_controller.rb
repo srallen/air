a083df2213425ad165b5f48f694bf0a8910cc27b
Sarah Allen <sarah.sra1@gmail.com>
2013-09-13 13:56:15 -0500

    Added session controller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by email: params[:email]
+
+    if @user.present && @user.authenticate(params(:password))
+      session[:user_id] = @user.id
+      flash[:notice] = 'Signed in successfully'
+      redirect_to root_url
+    else
+      # TODO add error handling
+      render 'new'
+    end
+  end
+
+  def destroy
+    reset_session
+    flash[:notice] = 'Signed out successfully'
+    redirect_to root_url
+  end
+
 end
