e93043be9267e7c0db309d30887fa4dd5b01e560
Sarah Allen <sarah.sra1@gmail.com>
2013-09-30 16:45:38 -0500

    Added miles earned functionality by custom model method.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,11 @@ class Reservation < ActiveRecord::Base
   belongs_to :flight
   belongs_to :user
 
+  after_create :update_miles_earned
+
+  def update_miles_earned
+    self.user.miles_earned += self.flight.distance
+    self.user.save
+  end
+
 end
