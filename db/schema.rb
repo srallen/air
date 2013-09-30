df81ff148319f82812884c074693e512d772b5a7
Sarah Allen <sarah.sra1@gmail.com>
2013-09-30 16:35:50 -0500

    Added miles earned to users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130930184914) do
+ActiveRecord::Schema.define(version: 20130930213349) do
 
   create_table "flights", force: true do |t|
     t.integer  "number"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20130930184914) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "miles_earned",    default: 0
   end
 
 end
