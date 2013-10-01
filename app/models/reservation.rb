e661f1ca2172cb671894c55aeae74fbaa751d82d
Sarah Allen <sarah.sra1@gmail.com>
2013-10-01 16:05:14 -0500

    Added miles_earned to users

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,15 +2,23 @@ class Reservation < ActiveRecord::Base
   belongs_to :flight
   belongs_to :user
 
-  after_create :update_miles_earned, :update_seats
+  after_create :update_miles_earned, :book_res_seats
+  after_destroy :cancel_res_seats
 
   def update_miles_earned
     self.user.miles_earned += self.flight.distance
     self.user.save
   end
 
-  def update_seats
+  # Decreases seat count for flight when reservation is booked by user.
+  def book_res_seats
     self.flight.seats -= 1
+    self.flight.save
+  end
+
+   # Increases seat count for flight when reservation is cancelled by user.
+  def cancel_res_seats
+    self.flight.seats += 1
     self.flight.save
   end
 
---
+++ db/migrate/20130930213349_add_miles_earned_to_users.rb
@@ -0,0 +1,5 @@
+class AddMilesEarnedToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :miles_earned, :integer, :default => 0
+  end
+end
